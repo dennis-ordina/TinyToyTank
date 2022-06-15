f6a39d68cbcb135a30e2e4e68c62f3d54e97dd64
Dennis van Oosten <dennis.van.oosten@ordina.nl>
2022-06-15 11:35:01 +0200

    Initial Commit

---
+++ TinyToyTank/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TinyToyTank
+//
+//  Created by Dennis van Oosten on 15/06/2022.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
