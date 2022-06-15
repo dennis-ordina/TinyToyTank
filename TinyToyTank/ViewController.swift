7cd67387a8e74089205ac6a1d09d16d8d5056a3a
Dennis van Oosten <dennis.van.oosten@ordina.nl>
2022-06-15 14:29:50 +0200

    1. AR quick start

--- TinyToyTank/ViewController.swift
+++ TinyToyTank/ViewController.swift
@@ -10,15 +10,63 @@ import RealityKit
 
 class ViewController: UIViewController {
     
+    var isActionPlaying: Bool = false
+    
     @IBOutlet var arView: ARView!
+    var tankAnchor: TinyToyTank._TinyToyTank?
+    
+    @IBAction func tankRightPressed(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { isActionPlaying = true }
+        
+        tankAnchor!.notifications.tankRight.post()
+    }
+    
+    @IBAction func tankLeftPressed(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { isActionPlaying = true }
+        
+        tankAnchor!.notifications.tankLeft.post()
+    }
+    
+    @IBAction func tankForwardPressed(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { isActionPlaying = true }
+        
+        tankAnchor!.notifications.tankForward.post()
+    }
+    
+    @IBAction func turretRightPressed(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { isActionPlaying = true }
+        
+        tankAnchor!.notifications.turretRight.post()
+    }
+    
+    @IBAction func turretLeftPressed(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { isActionPlaying = true }
+        
+        tankAnchor!.notifications.turretLeft.post()
+    }
+    
+    @IBAction func cannonFirePressed(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { isActionPlaying = true }
+        
+        tankAnchor!.notifications.cannonFire.post()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        tankAnchor = try! TinyToyTank.load_TinyToyTank()
+        tankAnchor!.cannon?.setParent(
+          tankAnchor!.tank, preservingWorldTransform: true)
+        tankAnchor!.actions.actionComplete.onAction = { _ in
+            self.isActionPlaying = false
+        }
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.scene.anchors.append(tankAnchor!)
     }
 }
